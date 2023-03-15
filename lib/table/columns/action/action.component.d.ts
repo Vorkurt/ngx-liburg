289147207b7265d3b60abdd4ec5d9f8ea58d1544
NsdHSO <37635083+NsdHSO@users.noreply.github.com>
2023-03-15 19:25:58 +0000

    Deploying to gh-pages from  @ 5632494c7db1ebb069358037e6430f280bef6f12 🚀

--- lib/table/columns/action/action.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnInit } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class ActionComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<ActionComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ActionComponent, "lib-action", never, {}, {}, never, never, false>;
-}
--- lib/table/columns/service/column-rotate.service.d.ts
+++
@@ -1,9 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class ColumnRotateService {
-    private readonly _rotate;
-    rotate$: import("rxjs").Observable<string>;
-    setSide(position: string): void;
-    swapColumn(doubleColumnToDisplay: string[]): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<ColumnRotateService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<ColumnRotateService>;
-}
